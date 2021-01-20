5a83d8f5fc7999f6c74cd80cba4b9d57c807adb4
Viking <37813004+Vikinges@users.noreply.github.com>
2021-01-20 22:33:15 +0200

    console e.pageX

---
+++ app.js
@@ -0,0 +1,8 @@
+// movement animation to happen 
+const cart = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+// moving animation Event 
+container.addEventListener('mousemove',(e)=>{
+console.log(e.pageX);
+});
