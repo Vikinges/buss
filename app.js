9b71fef25ae95cec37be09321224eb5bebd5d28c
Viking <37813004+Vikinges@users.noreply.github.com>
2021-01-21 01:07:57 +0200

    fixbug card

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // movement animation to happen 
-const cart = document.querySelector('.card');
+const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 
 // moving animation Event 
-container.addEventListener('mousemove',(e)=>{
-console.log(e.pageX);
+container.addEventListener("mousemove", (e) => {
+    // console.log(e.pageX, e.pageY);
+let xAxis = (window.innerWidth / 2 - e.pageX) / 10; 
+let yAxis = (window.innerHeight / 2 - e.pageY) / 10; 
+card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
+
