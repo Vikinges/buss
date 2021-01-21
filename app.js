243456211c4c209100393ff87e4cbc4ea7d688f3
Viking <37813004+Vikinges@users.noreply.github.com>
2021-01-21 17:12:57 +0200

    Make 3d effect

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 // movement animation to happen 
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+// titlre
+const title = document.querySelector('.title');
+const tur = document.querySelector('.tur img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const tips = document.querySelector('.tips');
 
 // moving animation Event 
 container.addEventListener("mousemove", (e) => {
     // console.log(e.pageX, e.pageY);
-let xAxis = (window.innerWidth / 2 - e.pageX) / 10; 
-let yAxis = (window.innerHeight / 2 - e.pageY) / 10; 
+let xAxis = (window.innerWidth / 2 - e.pageX) / 25; 
+let yAxis = (window.innerHeight / 2 - e.pageY) / 25; 
 card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
+// animate in 
+container.addEventListener("mouseenter",(e)=>{
+ card.style.transition = "none";
+ //Popout
+ title.style.transform ="translateZ(150px)";
+});
+// animate out 
+container.addEventListener("mouseleave", (e)=>{
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+// Popback
+title.style.transform ="translateZ(0px)";
+}); 
